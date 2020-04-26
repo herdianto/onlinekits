789a2230717155caae5a64e00643be0885d2d48a
Herdi <herdi.16@gmail.com>
2020-04-26 14:04:41 +0700

    update sicepat

--- src/interfaces/http/airwaybill/AirWayBillController.js
+++ src/interfaces/http/airwaybill/AirWayBillController.js
@@ -3,7 +3,7 @@ const { inject } = require('awilix-express');
 const Status = require('http-status');
 
 const AirWayBillController = {
-    
+
     get router() {
         const router = Router();
 
@@ -20,7 +20,7 @@ const AirWayBillController = {
     trackAnterajabyAyiip(req, res, next) {
         const axios = require('axios').default;
         axios({
-            url: "http://ayiip.com/tracking/anteraja.php?resi="+req.query.waybill,
+            url: "http://ayiip.com/tracking/anteraja.php?resi=" + req.query.waybill,
             timeout: 11000,
             method: 'GET'
         }).then(function (response) {
@@ -28,60 +28,92 @@ const AirWayBillController = {
             //console.log(response.data);
             res.send(response.data);
         })
-        .catch(function (error) {
-            console.log("error");
-            console.log(error.message);
-            res.send(typeof error.response != "undefined"?error.response.data:error.message);
-        });
+            .catch(function (error) {
+                console.log("error");
+                console.log(error.message);
+                res.send(typeof error.response != "undefined" ? error.response.data : error.message);
+            });
     },
     trackbyRajaOngkir(req, res, next) {
         const axios = require('axios').default;
         const responseData = {
-            
+
         }
-        axios({
-            url: "https://pro.rajaongkir.com/api/waybill",
-            timeout: 11000,
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json', 'key': 'c1958599c8d9171e1020b56eaeda20b3' },
-            data: {
-                "waybill": req.query.waybill,
-                "courier": req.query.courier
-            }
-        })
-            .then(function (response) {
+        if (req.query.courier === "anteraja") {
+            axios({
+                url: "http://ayiip.com/tracking/anteraja.php?resi=" + req.query.waybill,
+                timeout: 11000,
+                method: 'GET'
+            }).then(function (response) {
                 console.log("ok");
-                //console.log(response.data);
-                responseData.status = response.data.rajaongkir.status.code;
-                responseData.waybill = response.data.rajaongkir.query.waybill;
-                responseData.courier = response.data.rajaongkir.query.courier;
-                responseData.isDelivered = response.data.rajaongkir.result.delivered;
-                responseData.jenisPaket = response.data.rajaongkir.result.summary.service_code;
-                responseData.tanggalResi = response.data.rajaongkir.result.summary.waybill_date;
-                responseData.namaPengirim = response.data.rajaongkir.result.summary.shipper_name;
-                responseData.namaPenerima = response.data.rajaongkir.result.summary.receiver_name;
-                responseData.namaPenerimaAktual = response.data.rajaongkir.result.delivery_status.pod_receiver;
-                responseData.alamatAsal = response.data.rajaongkir.result.summary.origin +" "+
-                response.data.rajaongkir.result.details.shipper_address1 + " " +
-                response.data.rajaongkir.result.details.shipper_address2 + " " +
-                response.data.rajaongkir.result.details.shipper_address3; 
-                responseData.alamatTujuan = response.data.rajaongkir.result.summary.destination +" "+
-                response.data.rajaongkir.result.details.receiver_address1 + " " +
-                response.data.rajaongkir.result.details.receiver_address2 + " " +
-                response.data.rajaongkir.result.details.receiver_address3; 
-                responseData.tglTerima = response.data.rajaongkir.result.delivery_status.pod_date;
-                responseData.waktuTerima = response.data.rajaongkir.result.details.waybill_time;
-                responseData.tglKirim = response.data.rajaongkir.result.details.waybill_date;
-                responseData.waktuKirim = response.data.rajaongkir.result.delivery_status.pod_time;
-                responseData.manifest = response.data.rajaongkir.result.manifest;
+                responseData.status = response.status;
+                responseData.waybill = response.data.details.waybill_number;
+                responseData.courier = req.query.courier;
+                responseData.isDelivered = response.data.details.receiver_name === ""? false : true;
+                responseData.jenisPaket = response.data.details.service_code;
+                responseData.tanggalResi = response.data.details.waybill_time;
+                responseData.namaPengirim = response.data.details.shippper_name;
+                responseData.namaPenerima = response.data.details.receiver_name;
+                responseData.namaPenerimaAktual = response.data.details.receiver_name;
+                responseData.alamatAsal = response.data.details.shipper_city;
+                responseData.alamatTujuan = response.data.details.receiver_city;
+                responseData.tglTerima = "";
+                responseData.waktuTerima = "";
+                responseData.tglKirim = "";
+                responseData.waktuKirim = "";
+                responseData.manifest = "";
                 res.send(responseData);
             })
-            .catch(function (error) {
-                console.log("error");
-                console.log(error.message.status);
-                res.send(typeof error.response != "undefined"?error.response.data.rajaongkir.status:error.message);
-            });
-        
+                .catch(function (error) {
+                    console.log("error");
+                    console.log(error.message);
+                    res.send(typeof error.response != "undefined" ? error.response.data : error.message);
+                });
+        } else {
+            axios({
+                url: "https://pro.rajaongkir.com/api/waybill",
+                timeout: 11000,
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json', 'key': 'c1958599c8d9171e1020b56eaeda20b3' },
+                data: {
+                    "waybill": req.query.waybill,
+                    "courier": req.query.courier
+                }
+            })
+                .then(function (response) {
+                    console.log("ok");
+                    responseData.status = response.data.rajaongkir.status.code;
+                    responseData.waybill = response.data.rajaongkir.query.waybill;
+                    responseData.courier = response.data.rajaongkir.query.courier;
+                    responseData.isDelivered = response.data.rajaongkir.result.delivered;
+                    responseData.jenisPaket = response.data.rajaongkir.result.summary.service_code;
+                    responseData.tanggalResi = response.data.rajaongkir.result.summary.waybill_date;
+                    responseData.namaPengirim = response.data.rajaongkir.result.summary.shipper_name;
+                    responseData.namaPenerima = response.data.rajaongkir.result.summary.receiver_name;
+                    responseData.namaPenerimaAktual = response.data.rajaongkir.result.delivery_status.pod_receiver;
+                    responseData.alamatAsal = response.data.rajaongkir.result.summary.origin + " " +
+                        response.data.rajaongkir.result.details.shipper_address1 + " " +
+                        response.data.rajaongkir.result.details.shipper_address2 + " " +
+                        response.data.rajaongkir.result.details.shipper_address3;
+                    responseData.alamatTujuan = response.data.rajaongkir.result.summary.destination + " " +
+                        response.data.rajaongkir.result.details.receiver_address1 + " " +
+                        response.data.rajaongkir.result.details.receiver_address2 + " " +
+                        response.data.rajaongkir.result.details.receiver_address3;
+                    responseData.tglTerima = response.data.rajaongkir.result.delivery_status.pod_date;
+                    responseData.waktuTerima = response.data.rajaongkir.result.details.waybill_time;
+                    responseData.tglKirim = response.data.rajaongkir.result.details.waybill_date;
+                    responseData.waktuKirim = response.data.rajaongkir.result.delivery_status.pod_time;
+                    responseData.manifest = response.data.rajaongkir.result.manifest;
+                    res.send(responseData);
+                })
+                .catch(function (error) {
+                    console.log("error");
+                    console.log(error.message.status);
+                    res.send(typeof error.response != "undefined" ? error.response.data.rajaongkir.status : error.message);
+                });
+        }
+
+
     }
 };
 module.exports = AirWayBillController;
